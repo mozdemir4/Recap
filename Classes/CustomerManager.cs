6a407980afdfe3ef8050c69c734ee4d6f166b948
mozdemir4 <muhammt.ozdemir41@gmail.com>
2021-10-20 22:55:16 +0300

    Add project files.

---
+++ Classes/CustomerManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Classes
+{
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added!");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Customer Uptated");
+        }
+    }
+}
