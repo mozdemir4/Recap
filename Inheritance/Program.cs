59f0953a3d5aec228ebc2746f0e9070031d14a91
mozdemir4 <muhammt.ozdemir41@gmail.com>
2021-10-21 13:04:18 +0300

    Inheritance Demo

---
+++ Inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person[] people = new Person[]
+            {
+              new Student { FirstName="Muhammet",LastName="ÖZDEMİR"},
+              new Customer { FirstName="Elif",LastName="ÖZDEMİR"},
+              new Person{FirstName ="Semiha",LastName = "ÖZDEMİR"}
+            };
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.WriteLine(people[i].FirstName+" "+people[i].LastName);
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+    class Student : Person
+    {
+        public string School { get; set; }
+    }
+}
