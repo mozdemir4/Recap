916a3a7b39f60960702db3bb61c0b85b381e578c
mozdemir4 <muhammt.ozdemir41@gmail.com>
2021-10-22 23:00:11 +0300

    InterfaceAbstractDemo

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new CustomerCheckManager());
+
+            customerManager.Save(new Customer()
+            { FirstName = "Muhammet",LastName = "ÖZDEMİR",DateOfBirth = new DateTime(1997,2,20),NationaltyId="546516156",Id=1});
+        }
+    }
+
+   
+}
